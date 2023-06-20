94d07d9628a2646be32dda9437c0afe2025bbc30
Jorge Padilla <jorge.padilla29@gmail.com>
2023-06-20 19:57:57 -0400

    updated CORDS Header Domain

--- api/app.js
+++ api/app.js
@@ -12,9 +12,9 @@ const{ List, Task} = require('./db/models');
 //load middleware
 app.use(bodyParser.json());
 
-//CORS headers:
+//CORS HEADERS MIDDLEWARE:
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
